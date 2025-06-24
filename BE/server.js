9eee2567ee71376198aaa0e3ee6b28a708844fe4
Saverio Guardato <sa.guarda@gmail.com>
2025-06-24 23:25:09 +0200

    Serve FE directory

--- BE/server.js
+++ BE/server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+// Serve the front-end files located in the FE directory
+app.use(express.static(path.join(__dirname, '../FE')));
 
 // Ensure the uploads directory exists
 const uploadDir = path.join(__dirname, 'uploads');
